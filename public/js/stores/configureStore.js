afc382ab185c0d54e407f04d23a7119751526d97
Alun Turner <alun.turner@googlemail.com>
2020-09-05 10:32:24 +0100

    complete m6-02

--- public/js/stores/configureStore.js
+++ public/js/stores/configureStore.js
@@ -1,4 +1,5 @@
-import { createStore } from "redux";
+import { applyMiddleware, createStore } from "redux";
+import logger from "redux-logger";
 
 let defaultState = {
   originAmount: "0.00",
@@ -6,12 +7,11 @@ let defaultState = {
 
 function amountReducer(state = defaultState, action) {
   if (action.type === "CHANGE_ORIGIN_AMOUNT") {
-    console.log({ state });
     return { ...state, originAmount: action.data.newAmount };
   }
   return state;
 }
 
-let store = createStore(amountReducer);
+let store = createStore(amountReducer, applyMiddleware(logger));
 
 export default store;
